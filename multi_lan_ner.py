64c8004f8d91205260174c1e95ec5b861b032ae6
Janek <jpotworowski@gmail.com>
2021-06-17 18:05:29 +0200

    minor fix

--- multi_lan_ner.py
+++ multi_lan_ner.py
@@ -1,6 +1,4 @@
-import spacy
-
-def find_entities(intput, language):
+def find_entities(input_phrase, language):
   models = {
       'en': 'en_core_web_sm',
       'pl': 'pl_core_news_sm',
@@ -11,6 +9,7 @@ def find_entities(intput, language):
 
   if language in models:
     nlp = spacy.load(models[language])
+    doc = nlp(input_phrase)
     res = []
     for ent in doc.ents:
       res.append({'text': ent.text, 'start_pos': ent.start_char, 'end_pos': ent.end_char, 'type': ent.label_})
